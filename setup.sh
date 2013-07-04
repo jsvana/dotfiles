11dea868c10c5c7d4dd36a42b584775433927a49
Jay Vana <jsvana@mtu.edu>
2013-07-03 19:48:17 -0700

    Add setup

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Credit to @belak for the base of this
+
+function config_link {
+        if [[ ! -e ~/.$1 ]]
+        then
+                ln -s `pwd`/$1 ~/.$1
+        fi
+}
+
+config_link vimrc
+config_link vim
+config_link zshrc
+config_link gitconfig
+config_link tmux.conf
+
+# Stupid irssi
+mkdir -p ~/.irssi
+ln -s `pwd`/irssi.config ~/.irssi/config
+
+vim +BundleInstall +qall
