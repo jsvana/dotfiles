432fdd8a28638865f70984775ea515502301949d
Jay Vana <jsvana@mtu.edu>
2013-07-25 14:25:19 -0700

    Changes

--- setup.sh
+++ setup.sh
@@ -25,19 +25,20 @@ function aur_build {
 
 config_link gitconfig
 config_link hgrc
+
 config_link irssi
 config_link mutt
 config_link muttrc
 config_link newsbeuter
-config_link oh-my-zsh
+config_link slate
 config_link tmux.conf
 config_link vim
 config_link vimrc
 config_link wgetrc
 
 config_link zlogin
-config_link zshrc
 config_link zsh
+config_link zshrc
 
 bin_link pocket-cli
 
