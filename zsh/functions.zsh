5f36a56796524eef657167785bc9f2cc8f2b1a3d
Jay Vana <jsvana@mtu.edu>
2013-07-27 15:28:32 -0500

    More wat

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -14,42 +14,36 @@ function qc() {
 function battery() {
 	if [[ `uname -s` == "Darwin" ]]
 	then
-		local BATTPERCENT=$(ioreg -n AppleSmartBattery -r | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%.2f%%"; max=c["\"MaxCapacity\""]; print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}' | sed -E 's/(^[0-9]+).*/\1/')
-		local PR_BATTERY="asdf"
-		if [[ "${BATTPERCENT}" -lt 15 ]]
+		local percent=$(ioreg -n AppleSmartBattery -r | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%.2f%%"; max=c["\"MaxCapacity\""]; print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}' | sed -E 's/(^[0-9]+).*/\1/')
+		if [[ "${percent}" -lt 15 ]]
 		then
-			PR_BATTERY="${fg_red} ${BATTPRCNT}${at_normal}"
-		elif [[ "${BATTPERCENT}" -lt 50 ]]
+			batt="${fg_red} ${percent}${at_normal}"
+		elif [[ "${percent}" -lt 50 ]]
 		then
-			PR_BATTERY="${fg_yellow} ${BATTPRCNT}${at_normal}"
-		elif [[ "${BATTPERCENT}" -lt 96 ]]
+			batt="${fg_yellow} ${percent}${at_normal}"
+		elif [[ "${percent}" -lt 96 ]]
 		then
-			PR_BATTERY="${fg_green} ${BATTPRCNT}${at_normal}"
+			batt="${fg_green} ${percent}${at_normal}"
 		else
-			PR_BATTERY="foobar"
+			batt="foobar"
 		fi
-		print "$PR_BATTERY"
+		echo "$batt"
 	elif which acpi &> /dev/null
 	then
-		local BATTSTATE="$(acpi -b)"
-		local BATTPRCNT="$(echo ${BATTSTATE[(w)4]} | sed -r 's/(^[0-9]+).*/\1/')"
-		if [[ -z "${BATTPRCNT}" ]]
+		local state=$(acpi -b)
+		local percent=$(echo ${state[(w)4]} | sed -r 's/(^[0-9]+).*/\1/')
+		elif [[ "${percent}" -lt 15 ]]
 		then
-			PR_BATTERY=""
-		elif [[ "${BATTPRCNT}" -lt 15 ]]
-		then
-			PR_BATTERY="${PR_BOLD_RED} batt:${BATTPRCNT}%%"
+			batt="${fg_red} ${percent}${at_normal}"
 		elif [[ "${BATTPRCNT}" -lt 50 ]]
 		then
-			PR_BATTERY="${PR_BOLD_YELLOW} batt:${BATTPRCNT}%%"
-		elif [[ "${BATTPRCNT}" -lt 96 ]]
+			batt="${fg_yellow} ${percent}${at_normal}"
+		elif [[ "${percent}" -lt 96 ]]
 		then
-			PR_BATTERY=" batt:${BATTPRCNT}%%"
+			batt="${fg_green} ${percent}${at_normal}"
 		else
-			PR_BATTERY=""
+			batt=""
 		fi
-		echo $PR_BATTERY
-	else
-		echo ""
+		echo $batt
 	fi
 }
