258560fa6712afa91d70d30ae29665ae8c9c728f
Jay Vana <jsvana@mtu.edu>
2013-07-30 13:49:21 -0400

    Update irssi_fnotify

--- bin/irssi_fnotify.sh
+++ bin/irssi_fnotify.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-ssh jsvana@jsvana.io -p 49621 -o PermitLocalCommand=no  \
-  "touch .irssi/fnotify ; tail -f .irssi/fnotify " |  \
-while read heading message; do                      \
-  growlnotify -s -t "${heading}" -m "${message}";      \
+ssh hephaestus -o PermitLocalCommand=no "touch .irssi/fnotify ; tail -f .irssi/fnotify " | \
+while read heading message; do
+  growlnotify -s -t "${heading}" -m "${message}"
 done
