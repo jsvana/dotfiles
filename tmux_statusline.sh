df7010442712b0ac1c7849829d67d94d051bb300
Jay Vana <jsvana@mtu.edu>
2015-01-12 09:04:04 -0800

    Add mercurial bookmark to tmux statusline

--- tmux_statusline.sh
+++ tmux_statusline.sh
@@ -43,51 +43,59 @@ git_pwd() {
   printf " #[fg=$color]%s" "$(short_pwd)"
 }
 
-wifi() {
-	local state="$(airport -I | grep state | awk '{print $2; }')"
+hg_branch() {
+  printf " #[fg=colour5]$(hg bookmark | grep '*' | awk '{ print $2; }') "
+}
 
-	printf " #[fg=colour11]"
-	if [[ -n "$state" ]]; then
-		if [[ "$state" -eq "running" ]]; then
-			printf "+"
-		else
-			printf "±"
-		fi
-	else
-		printf "-"
-	fi
+wifi() {
+  local state="$(airport -I | grep state | awk '{print $2; }')"
+
+  printf " #[fg=colour11]"
+  if [[ -n "$state" ]]; then
+    if [[ "$state" -eq "running" ]]; then
+      printf "+"
+    else
+      printf "±"
+    fi
+  else
+    printf "-"
+  fi
 }
 
 battery() {
-	if which acpi >/dev/null; then
-		printf " #[fg=colour6]%s" "$(acpi -b | sed -r 's/^.*, ([0-9]+)%.*/\1/')%"
+  if which acpi >/dev/null; then
+    printf " #[fg=colour6]%s" "$(acpi -b | sed -r 's/^.*, ([0-9]+)%.*/\1/')%"
 
-		if acpi -b | grep -E '(Charging|Full)' &> /dev/null; then
-			printf " \u26a1"
-		fi
-	else
-		printf " #[fg=colour6]%s" "$(pmset -g ps  |  sed -n 's/.*[[:blank:]]+*\(.*%\).*/\1/p')"
-	fi
+    if acpi -b | grep -E '(Charging|Full)' &> /dev/null; then
+      printf " \u26a1"
+    fi
+  else
+    printf " #[fg=colour6]%s" "$(pmset -g ps  |  sed -n 's/.*[[:blank:]]+*\(.*%\).*/\1/p')"
+  fi
 }
 
 date_f() {
-	printf " #[fg=colour9]%s" "$(date +"%d %b %H:%M")"
+  printf " #[fg=colour9]%s" "$(date +"%d %b %H:%M")"
 }
 
 ssh_conn() {
-	if [[ -n "$SSH_CONNECTION" ]]; then
-		printf " #[fg=colour9](ssh)"
-	fi
+  if [[ -n "$SSH_CONNECTION" ]]; then
+    printf " #[fg=colour9](ssh)"
+  fi
 }
 
 pacman_updates() {
-	if which pacman >/dev/null; then
-		printf " #[fg=colour25]%s updates" "$(cat /tmp/pacman)"
-	fi
+  if which pacman >/dev/null; then
+    printf " #[fg=colour25]%s updates" "$(cat /tmp/pacman)"
+  fi
 }
 
 tmux_session_name() {
   printf " #[fg=colour13]%s" "$(tmux display-message -p '#S')"
+}
+
+normal_pwd() {
+  printf " #[fg=colour4]%s" "$(short_pwd)"
 }
 
 ssh_conn
@@ -101,6 +109,9 @@ if git root &> /dev/null; then
   git_since
   git_branch
   git_pwd
+elif hg root &> /dev/null; then
+  hg_branch
+  normal_pwd
 else
-	printf " #[fg=colour4]%s" "$(short_pwd)"
+  normal_pwd
 fi
